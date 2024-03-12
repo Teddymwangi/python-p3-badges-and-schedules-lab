61617c1eb1d044e89f7c02defb98fa28f20c957b
Teddy mwangi <teddy.maina@student.moringaschool.com>
2024-03-12 11:24:04 +0300

    done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
+
+
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    return ["Hello, {}! You'll be assigned to room {}!".format(speaker, index + 1) for index, speaker in enumerate(speakers)]
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    for badge in badges:
+        print(badge)
+    for assignment in room_assignments:
+        print(assignment)
